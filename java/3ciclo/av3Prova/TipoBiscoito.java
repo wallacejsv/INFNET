44692fa6ac2d60be0f5ee20b494a327b211cebae
Jonathan Vieira <wallacejsv@gmail.com>
2015-04-07 15:40:02 -0300

    Create TipoBiscoito.java

---
+++ java/3ciclo/av3Prova/TipoBiscoito.java
@@ -0,0 +1,17 @@
+
+public enum TipoBiscoito {
+
+	D("DOCE"),
+	S("Salgado"),
+	FA("Forma A"),
+	FB("Forma B"),
+	FC("Forma C"),
+	FD("Forma D");
+	
+	String nome;
+	
+	TipoBiscoito(String tipoBiscoito) {
+		nome =  tipoBiscoito;
+	}
+	
+}
