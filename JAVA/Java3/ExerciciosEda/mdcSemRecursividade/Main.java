34d037225f5a8da228e98b2f800228d7c23168e8
Jonathan Vieira <wallacejsv@gmail.com>
2015-05-13 10:41:15 -0300

    Create Main.java

---
+++ JAVA/Java3/ExerciciosEda/mdcSemRecursividade/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Mdc m = new Mdc(0,0);
+		
+		System.out.println("Maior divisor comum: " + m.maiorDivisor(0, 0));
+
+	}
+
+}
