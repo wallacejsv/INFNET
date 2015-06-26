6be949a647e79fbf2388ce104d8e3cea8633bba6
Jonathan Vieira <wallacejsv@gmail.com>
2015-06-25 23:34:11 -0300

    Create Bem.java

---
+++ JAVA/Java3/provinha/com.bem/Bem.java
@@ -0,0 +1,22 @@
+package com.bem;
+
+public abstract class Bem {
+
+	protected String nome;
+	protected double valor;
+	
+	//encapsulamento
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+}
