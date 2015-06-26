f699a2a3841c92444c9bfec670d65bbbefc97613
Jonathan Vieira <wallacejsv@gmail.com>
2015-06-25 23:33:24 -0300

    Create Veiculo.java

---
+++ JAVA/Java3/provinha/com.bem/Veiculo.java
@@ -0,0 +1,16 @@
+package com.bem;
+
+public class Veiculo extends Bem {
+
+	private String chassi;
+
+	//encapsulamento
+	public String getChassi() {
+		return chassi;
+	}
+
+	public void setChassi(String chassi) {
+		this.chassi = chassi;
+	}
+	
+}
